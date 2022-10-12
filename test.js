2b337c65236a24985e7dc4990cdfbba16279ca4a
viverk <viverkange@live.fr>
2022-10-12 17:01:47 +0200

    fin de l'exo 4

--- test.js
+++ test.js
@@ -21,7 +21,7 @@ function somme(nombre1 = 0, nombre2 = 0) {
 describe("le programme devrait", function () {
   it("afficher la somme de deux chiffre sans la fonction somme()", function () {
     resultat = 5 + 5;
-    assert.strictEqual(resultat, 10);
+    assert.equal(resultat, 10);
   });
 });
 
@@ -29,13 +29,63 @@ describe("le programme devrait", function () {
 //test avec la fonction somme(param1,param2)
 describe("le programme devrait", () => {
   it(`afficher la somme de deux chiffre`, async () => {
-    assert.strictEqual(somme(1, 2), 3);
+    assert.equal(somme(1, 2), 3);
+  });
+});
+
+describe("le programme devrait", () => {
+  it(`afficher la somme de deux chiffre`, async () => {
+    assert.equal(somme(1, 2), 10);
   });
 });
 
 //====================================EXO4=====================================================================
+describe("le programme devrait", () => {
+  it(`afficher nom`, async () => {
+    var MongoClient = require("mongodb").MongoClient;
+    MongoClient.connect(
+      "mongodb+srv://mangeonsensemble:projet_pim_estiam@cluster0.svlza.mongodb.net/test",
 
-//====================================EXO5=====================================================================
+      (err, client) => {
+        // Client returned
+
+        var db = client.db("test");
+      }
+    );
+
+    MongoClient.connect(
+      "mongodb+srv://mangeonsensemble:projet_pim_estiam@cluster0.svlza.mongodb.net/test",
+
+      async function (err, client) {
+        if (err) throw err;
+
+        var db = client.db("test");
+
+        var db = client.db("test");
+
+        var users = db.collection("test");
+
+        const count = await users.countDocuments({});
+
+        /* console.log(`Nombre de documents : ${count}`); */
+
+        db.collection("test")
+
+          .find({})
+
+          .toArray(function (err, result) {
+            if (err) throw err;
+
+            /* console.log(result); */
+            console.log(result[0].name);
+            assert.equal(result[0].name, "Maxime");
+
+            client.close();
+          });
+      }
+    );
+  });
+});
 
 //====================================EXO6=====================================================================
 
